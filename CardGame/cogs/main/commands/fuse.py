0ddf8d488985b29967fc37f9eeb355c1359c041d
Amir <amir.ghaffarie@gmail.com>
2023-02-11 01:38:06 +0330

    fuse fix 3

--- CardGame/cogs/main/commands/fuse.py
+++ CardGame/cogs/main/commands/fuse.py
@@ -10,7 +10,7 @@ async def command(self, ctx, *args):
         if not legit:
             await ctx.send("Wrong usage of command.")
             return
-        all_cards = cards
+        all_cards = cards.copy()
         all_cards.append(main_card)
         for card in all_cards:
             async with session.get(
@@ -25,12 +25,14 @@ async def command(self, ctx, *args):
         xp_gains = []
         for card in cards:
             async with session.get(
-                f"{DB_BASE_ADDRESS}/fuse/{id}/{main_card}/{card}/"
+                f"{DB_BASE_ADDRESS}/fuse/{ctx.author.id}/{main_card}/{card}/"
             ) as r:
                 xp_gains.append(await r.text())
         embed = discord.Embed(title="Fuse", color=0xF78589)
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
-        embed_description = f"{ctx.author.mention} Fused in **{main_card}** the following cards\n"
+        embed_description = (
+            f"{ctx.author.mention} Fused in **{main_card}** the following cards\n"
+        )
         for (card, xp) in (cards, xp_gains):
             embed_description += f"{card} - {xp} XP\n"
         embed_description += f"Total {sum(xp_gains)/10} Levels Gained"
@@ -46,4 +48,4 @@ def get_infos(ctx, *args):
         if get_input_type(args[i]) != Inputs.Card:
             return 0, 0, False
         cards.append(get_card(args[i]))
-    return get_card(args[1]), cards, True
+    return get_card(args[0]), cards, True
