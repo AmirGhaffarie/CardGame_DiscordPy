2a4990f32d820aa147ccce01ae481202f982cd7f
Amir <amir.ghaffarie@gmail.com>
2023-09-05 00:11:06 +0330

    view fix

--- CardGame/cogs/main/commands/view.py
+++ CardGame/cogs/main/commands/view.py
@@ -7,21 +7,18 @@ from utilities.constants import *
 from utilities.functions import (
     get_image,
     get_card,
-    get_user,
-    get_input_type,
-    Inputs,
 )
 
 
 async def command(self, ctx, *args):
     async with ClientSession() as session:
-        id, card, level, legit = getInfos(ctx, *args)
+        card, legit = getInfos(ctx, *args)
         if not legit:
             await ctx.send("Wrong use of command.")
             return
         async with session.get(f"{DB_BASE_ADDRESS}/viewcard/{id}/{card}") as r:
             if r.status == 404:
-                await ctx.send("Player not exist or does not have the card.")
+                await ctx.send("Card Does not exist.")
             elif r.status == 210:
                 await ctx.send(await r.text())
             else:
@@ -39,25 +36,5 @@ async def command(self, ctx, *args):
 
 def getInfos(ctx, *args):
     if len(args) == 1:
-        return ctx.author.id, get_card(args[0]), 0, True
-    elif len(args) == 2:
-        arg1Type = get_input_type(args[0])
-        arg2Type = get_input_type(args[1])
-        if arg1Type == Inputs.User and arg2Type == Inputs.Card:
-            return get_user(args[0]), get_card(args[1]), 0, True
-        if arg1Type == Inputs.Card and arg2Type == Inputs.Number:
-            return ctx.author.id, get_card(args[0]), args[1], True
-    elif len(args) == 3:
-        arg1Type = get_input_type(args[0])
-        arg2Type = get_input_type(args[1])
-        arg3Type = get_input_type(args[2])
-        if (
-            arg1Type == Inputs.User
-            and arg2Type == Inputs.Card
-            and arg3Type == Inputs.Number
-        ):
-            return get_user(args[0]), get_card(args[1]), args[2], True
-        else:
-            return 0, 0, 0, False
-    else:
-        return 0, 0, 0, False
+        return get_card(args[0]), True
+    return 0, False
