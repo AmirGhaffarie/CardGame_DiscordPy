e9000597682c71f7723cd51a64cffd0f25ed311e
Amir <amir.ghaffarie@gmail.com>
2023-09-26 02:24:32 +0330

    embed reload added and hug :D

--- CardGame/cogs/main/commands/hug.py
+++ CardGame/cogs/main/commands/hug.py
@@ -1,13 +1,20 @@
+import asyncio
 import json, random
 
 from aiohttp.client import ClientSession
 
 
-async def command(self, ctx):
+async def command(self, ctx, *args):
     async with ClientSession() as session:
         async with session.get(
             "https://g.tenor.com/v1/random?q=kpophug&key=LIVDSRZULELA"
         ) as r:
             res = json.loads(await r.text())
             rnd = random.randrange(20)
-            await ctx.send(res[rnd]["media"][0]["gif"]["url"])
+            if len(args) > 0:
+                result = f"{ctx.author.display_name} hugged {args[0]}."
+            else:
+                result = f"{ctx.author.display_name} hugged air."
+            await ctx.send(
+                result + "\n" + res["results"][rnd]["media"][0]["gif"]["url"]
+            )
