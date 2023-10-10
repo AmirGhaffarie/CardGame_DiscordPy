4ebcc7eb9a740cd722f7d94207d13462477e5ccf
Amir <amir.ghaffarie@gmail.com>
2023-10-11 00:46:03 +0330

    add discovery to reload

--- CardGame/cogs/main/commands/reload.py
+++ CardGame/cogs/main/commands/reload.py
@@ -1,8 +1,9 @@
-from datas import emojis, localization, embeds
+from datas import emojis, localization, embeds, discoveries
 
 
 async def command(self, ctx):
     await embeds.load()
     await emojis.load()
     await localization.load()
+    await discoveries.load()
     await ctx.send("Reload Completed")
